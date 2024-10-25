8666135d1167feb2fa62cbd9b0b411d5ab8a0405
Afia Anjum <56650056+Afia-Anjum@users.noreply.github.com>
2024-10-25 12:27:41 -0600

    Update leetcode_2351_first_letter_to_appear_twice.py

--- leetcode_2351_first_letter_to_appear_twice.py
+++ leetcode_2351_first_letter_to_appear_twice.py
@@ -1,10 +1,19 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
         dict={}
-        s=list(s)
         for i in range(len(s)):
             if s[i] in dict:
                 dict[s[i]]=dict[s[i]]+1
                 return s[i]
             else:
                 dict[s[i]]=1
+
+####with a set
+# class Solution:
+#     def repeatedCharacter(self, s: str) -> str:
+#         seen=set()
+#         for i in range(len(s)):
+#             if s[i] in seen:
+#                 return s[i]
+#             else:
+#                 seen.add(s[i])
